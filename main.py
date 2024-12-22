d74b0bf3d6f71ffe7c150f857f7daea6e9fc81d6
Falon Freed <165129172+ffreed101@users.noreply.github.com>
2024-12-22 09:14:55 -0600

    implemented user selection and user management menus

--- main.py
+++ main.py
@@ -31,11 +31,58 @@ def remove_user(user):
 
 def get_user_by_name(first_name):
     cur.execute("SELECT * FROM users WHERE first=:first", {"first": first_name})
-    return cur.fetchall()
+    return cur.fetchone()
 
-user_1 = User("Falon", "Freed", 300)
-user_2 = User("Abigail", "Freed", 350)
+def create_user():
+    first = input("Enter first name: ").capitalize()
+    last = input("Enter last name: ").capitalize()
+    init_balance = float(input("Enter initial balance: "))
+    insert_user(User(first, last, init_balance))
 
-print(get_user_by_name("Falon"))
+def select_user():
+    while True:
+        selected = input("Enter user's first name: ").capitalize()
+        try:
+            attribute_list = get_user_by_name(selected)
+            return User(attribute_list[0], attribute_list[1], attribute_list[2])
+        except:
+            print("User not found.")
+
+def user_menu():
+    user = select_user()
+    while True:
+        print(f"{user.first} {user.last}")
+        print(f"Balance: {user.balance:.2f}")
+        print("1. Edit balance")
+        print("2. Delete User")
+        print("3. Back")
+        choice = int(input("Enter a choice: "))
+        match choice:
+            case 1:
+                new_balance = float(input("Enter new balance: "))
+                user.balance = new_balance
+                update_balance(user, user.balance)
+                
+            case 2:
+                remove_user(user)
+                break
+            case 3:
+                break
+
+def main():
+    while True:
+        print("Main Menu")
+        print("1. Log In")
+        print("2. Create user")
+        print("3. Exit")
+        choice = int(input("Enter a choice: "))
+        match choice:
+            case 1:
+                user_menu()
+            case 2:
+                create_user()
+            case 3:
+                break
+main()
 
 con.close()
