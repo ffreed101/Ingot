9f08458ba016680ceb39c1d85a0d50a903035722
Falon Freed <165129172+ffreed101@users.noreply.github.com>
2024-12-22 09:55:30 -0600

    Added list all users function and added fixed expenses to data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
+import json
 from models.User import User
 
+# SQLite functionality
 con = sqlite3.connect("database.db")
 cur = con.cursor()
 
@@ -8,7 +10,8 @@ try:
     cur.execute("""CREATE TABLE users (
                 first text,
                 last text,
-                balance real
+                balance real,
+                fixed_expenses text
         )""")
     print("Formatting data...")
 except:
@@ -16,13 +19,19 @@ except:
 
 def insert_user(user):
     with con:
-        cur.execute("INSERT INTO users VALUES (:first, :last, :balance)", {"first": user.first, "last": user.last, "balance": user.balance})
+        cur.execute("INSERT INTO users VALUES (:first, :last, :balance, :fixed_expenses)", {"first": user.first, "last": user.last, "balance": user.balance, "fixed_expenses": user.fixed_expenses_str})
 
 def update_balance(user, balance):
     with con:
         cur.execute("""UPDATE users SET balance = :balance 
                     WHERE first = :first AND last = :last""", 
                     {'first': user.first, 'last': user.last, 'balance': balance})
+        
+def update_fixed_expenses(user, fixed_expenses):
+    with con:
+        cur.execute("""UPDATE users SET fixed_expenses = :fixed_expenses 
+                    WHERE first = :first AND last = :last""", 
+                    {'first': user.first, 'last': user.last, 'fixed_expenses': user.fixed_expenses_str})
 
 def remove_user(user):
     with con:
@@ -33,18 +42,28 @@ def get_user_by_name(first_name):
     cur.execute("SELECT * FROM users WHERE first=:first", {"first": first_name})
     return cur.fetchone()
 
+# Utility functions
+def list_users():
+    cur.execute("SELECT first, last FROM users")
+    users = cur.fetchall()
+    for i, user in enumerate(users, 1):
+        print(f"{i}. {user[0]} {user[1]}")
+    return users
+
+# Program functionality
 def create_user():
     first = input("Enter first name: ").capitalize()
     last = input("Enter last name: ").capitalize()
     init_balance = float(input("Enter initial balance: "))
-    insert_user(User(first, last, init_balance))
+    fixed_expenses = get_fixed_expenses()
+    insert_user(User(first, last, init_balance, fixed_expenses, ))
 
 def select_user():
     while True:
         selected = input("Enter user's first name: ").capitalize()
         try:
             attribute_list = get_user_by_name(selected)
-            return User(attribute_list[0], attribute_list[1], attribute_list[2])
+            return User(attribute_list[0], attribute_list[1], attribute_list[2], json.loads(attribute_list[3]))
         except:
             print("User not found.")
 
@@ -69,12 +88,23 @@ def user_menu():
             case 3:
                 break
 
+def get_fixed_expenses():
+    expenses = {}
+    while True:
+        name = input("Enter expense name(Leave blank when finished): ")
+        if name == "":
+            break
+        amount = float(input("Enter expense amount: "))
+        expenses[name] = amount
+    return expenses
+
 def main():
     while True:
         print("Main Menu")
         print("1. Log In")
         print("2. Create user")
-        print("3. Exit")
+        print("3. List all Users")
+        print("4. Exit")
         choice = int(input("Enter a choice: "))
         match choice:
             case 1:
@@ -82,7 +112,9 @@ def main():
             case 2:
                 create_user()
             case 3:
+                list_users()
+            case 4:
                 break
-main()
 
+main()
 con.close()
--- models/User.py
+++ models/User.py
@@ -1,5 +1,9 @@
+import json
+
 class User:
-    def __init__(self, first, last, balance):
+    def __init__(self, first, last, balance, fixed_expenses):
         self.first = first
         self.last = last
         self.balance = balance
+        self.fixed_expenses = fixed_expenses
+        self.fixed_expenses_str = json.dumps(fixed_expenses)
