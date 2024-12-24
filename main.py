423f6dc8a03395aa20293dc2a2f5f2972e7c23a2
Falon Freed <165129172+ffreed101@users.noreply.github.com>
2024-12-24 17:55:44 -0600

    Fixed small bugs and began implementing transactions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import json
 from models.User import User
-
+from models.Transaction import Transaction
 # TODO: Add a transactions attribute to user class that modifies the balance, as well as adding user-side functionality to add, edit, view, and remove transactions in user_menu. Transactions might be separate data table
 
 # SQLite functionality
@@ -10,18 +10,33 @@ cur = con.cursor()
 
 try:
     cur.execute("""CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 first text,
                 last text,
                 balance real,
                 fixed_expenses text
         )""")
-    print("Formatting data...")
+    print("Formatting user data...")
 except:
-    print("Data found")
+    print("User data found")
+
+try:
+    cur.execute("""CREATE TABLE transactions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                date TEXT,
+                category TEXT,
+                amount REAL,
+                note TEXT,
+                FOREIGN KEY (user_id) REFERENCES users (rowid)
+        )""")
+    print("Formatting transactions data...")
+except:
+    print("Transactions data found")
 
 def insert_user(user):
     with con:
-        cur.execute("INSERT INTO users VALUES (:first, :last, :balance, :fixed_expenses)", {"first": user.first, "last": user.last, "balance": user.balance, "fixed_expenses": user.fixed_expenses_str})
+        cur.execute("INSERT INTO users (first, last, balance, fixed_expenses) VALUES (:first, :last, :balance, :fixed_expenses)", {"first": user.first, "last": user.last, "balance": user.balance, "fixed_expenses": user.fixed_expenses_str})
 
 def update_balance(user, balance):
     with con:
@@ -44,13 +59,35 @@ def get_user_by_name(first_name):
     cur.execute("SELECT * FROM users WHERE first=:first", {"first": first_name})
     return cur.fetchone()
 
+def get_user_by_id(user_id):
+    cur.execute("SELECT * FROM users WHERE id=:id", {"id": user_id})
+    return cur.fetchone()
+
+# Transactions CRUD
+
+def insert_transaction(transaction):
+    with con:
+        cur.execute("""INSERT INTO transactions (user_id, date, category, amount, note)
+                       VALUES (:user_id, :date, :category, :amount, :note)""",
+                    {'user_id': transaction.user_id,
+                     'date': transaction.date,
+                     'category': transaction.category,
+                     'amount': transaction.amount,
+                     'note': transaction.note})
+
+def get_transactions_by_user(user_id):
+    cur.execute("SELECT * FROM transactions WHERE user_id = :user_id", {"user_id": user_id})
+    return cur.fetchall()
+
+def delete_transaction(transaction_id):
+    with con:
+        cur.execute("DELETE FROM transactions WHERE id = :transaction_id", {"transaction_id": transaction_id})
+
+
+
 # Utility functions
 def list_users():
-    cur.execute("SELECT first, last FROM users")
-    users = cur.fetchall()
-    for i, user in enumerate(users, 1):
-        print(f"{i}. {user[0]} {user[1]}")
-    return users
+    print("Not Implemented")
 
 # Program functionality
 def create_user():
@@ -58,37 +95,43 @@ def create_user():
     last = input("Enter last name: ").capitalize()
     init_balance = float(input("Enter initial balance: "))
     fixed_expenses = get_fixed_expenses()
-    insert_user(User(first, last, init_balance, fixed_expenses, ))
+    insert_user(User(first, last, init_balance, fixed_expenses))
 
 def select_user():
     while True:
-        selected = input("Enter user's first name: ").capitalize()
-        try:
-            attribute_list = get_user_by_name(selected)
-            return User(attribute_list[0], attribute_list[1], attribute_list[2], json.loads(attribute_list[3]))
-        except:
-            print("User not found.")
+        selected = input("Enter user's first name(Leave blank to go back): ").capitalize()
+        if selected == '':
+            return None
+        else:
+            try:
+                attribute_list = get_user_by_name(selected)
+                return User(attribute_list[1], attribute_list[2], attribute_list[3], json.loads(attribute_list[4]))
+            except:
+                print("User not found.")
 
 def user_menu():
     user = select_user()
-    while True:
-        print(f"{user.first} {user.last}")
-        print(f"Balance: {user.balance:.2f}")
-        print("1. Edit balance")
-        print("2. Delete User")
-        print("3. Back")
-        choice = int(input("Enter a choice: "))
-        match choice:
-            case 1:
-                new_balance = float(input("Enter new balance: "))
-                user.balance = new_balance
-                update_balance(user, user.balance)
-                
-            case 2:
-                remove_user(user)
-                break
-            case 3:
-                break
+    if user != None:
+        while True:
+            print(f"{user.first} {user.last}")
+            print(f"Balance: {user.balance:.2f}")
+            print("1. Edit balance")
+            print("2. Delete User")
+            print("3. Back")
+            choice = int(input("Enter a choice: "))
+            match choice:
+                case 1:
+                    new_balance = float(input("Enter new balance: "))
+                    user.balance = new_balance
+                    update_balance(user, user.balance)
+                    
+                case 2:
+                    remove_user(user)
+                    break
+                case 3:
+                    break
+    else:
+        pass
 
 def get_fixed_expenses():
     expenses = {}
--- models/Transaction.py
+++ models/Transaction.py
@@ -1,2 +1,7 @@
-class Transactions:
-    pass
+class Transaction:
+    def __init__(self, user_id, date, category, amount, note=""):
+        self.user_id = user_id
+        self.date = date
+        self.category = category
+        self.amount = amount
+        self.note = note
